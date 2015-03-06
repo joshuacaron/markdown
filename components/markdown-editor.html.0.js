1f25b038e84dbe2a6cadf52f1514b39b9c12a0bf
Joshua Caron <jocaron@gmail.com>
2015-03-06 07:32:51 -0500

    copies html on click

--- components/markdown-editor.html.0.js
+++ components/markdown-editor.html.0.js
@@ -1,10 +1,4 @@
 
     Polymer('markdown-editor', {
-      markdown:"Type in *Markdown* and see the result **instantly**.",
-      markdownChanged:function(x,y){
-        this.injectBoundHTML(markdown.toHTML(this.markdown),this.$.preview);
-        // this.fire('markdown-updated',{
-        //   "markdown":this.markdown
-        // })
-      }
+      markdown:"",
     });
