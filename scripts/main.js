97695a5da1ba78f963ee76e2170f516f8720eafb
Joshua Caron <jocaron@gmail.com>
2015-03-05 23:16:37 -0500

    added support for loading fonts from google fonts

--- scripts/main.js
+++ scripts/main.js
@@ -21,4 +21,23 @@ document.addEventListener('DOMContentLoaded', function(){
     elem.addEventListener('click',function(){
     	newPage();
     });
+
+    $(window).bind('keydown', function(event) {
+    if (event.ctrlKey || event.metaKey) {
+        switch (String.fromCharCode(event.which).toLowerCase()) {
+        case 's':
+            event.preventDefault();
+            console.log('ctrl-s');
+            break;
+        // case 'f':
+        //     event.preventDefault();
+        //     console.log('ctrl-f');
+        //     break;
+        // case 'g':
+        //     event.preventDefault();
+        //     console.log('ctrl-g');
+        //     break;
+        }
+    }
+});
 });
